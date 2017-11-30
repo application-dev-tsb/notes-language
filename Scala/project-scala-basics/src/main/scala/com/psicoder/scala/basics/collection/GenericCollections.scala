05e6847d613faff01743481546e63d34052d6c16
Lyndon Michael Bibera <lyndon.bibera@boltoninternational.com>
2017-11-30 17:57:42 +0800

    scala: collections

---
+++ Scala/project-scala-basics/src/main/scala/com/psicoder/scala/basics/collection/GenericCollections.scala
@@ -0,0 +1,13 @@
+package com.psicoder.scala.basics.collection
+
+object GenericCollections {
+  def main(args: Array[String]): Unit = {
+    val list = List("a", "b", "c")
+
+    println(s"List: ${list.mkString("-")}")
+
+    val map = Map("a" -> "Awe", "b" -> "Boo")
+
+    println(s"Map Item Found ${map get "c" getOrElse "Not Found"}")
+  }
+}
