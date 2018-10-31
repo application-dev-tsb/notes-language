01f553d76edb043ccecabd1a609b5f406d8d329d
LM Bibera <secret.transaction@gmail.com>
2018-11-01 07:46:45 +0800

    generated a JDK 11 spring project as a playground for the new features

---
+++ Java/projects/java-11/src/main/java/com/tsb/java/java11/Java11Application.java
@@ -0,0 +1,12 @@
+package com.tsb.java.java11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java11Application.class, args);
+	}
+}
